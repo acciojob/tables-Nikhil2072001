7aaf1bff278189ccfd9acc8715a78a27438ad755
Nikhil2072001 <116430126+Nikhil2072001@users.noreply.github.com>
2023-03-01 23:03:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your code here
+function TotalFormatter (items) {
+ var totalPrice = 0;
+ items.forEach(function (item) {
+  totalPrice = parseFloat(totalPrice) + parseFloat(item.Amount.replace('$','').replace(',',''));
+ });
+
+ return '$' + parseFloat(totalPrice).toLocaleString(undefined, {
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2
+ });
+};
